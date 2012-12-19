f970d5b8efc9410fd2c5479d63011cf37fabf4ac
Pedro González Serrano <niteman.es@gmail.com>
2012-12-19 19:43:35 +0100

    intended refactor

---
+++ manifests/server/configmatch.pp
@@ -0,0 +1,41 @@
+define ssh::server::configmatch (
+  $ensure    = present,
+  $condition = false,
+  $settings  = {},
+) {
+  include ssh::server
+
+  # TO-DO: Make it work with augeas
+  Augeas {
+    context => "/files${ssh::params::sshd_config}",
+    notify  => Class['ssh::server::service'],
+    require => Class['ssh::server::config'],
+  }
+
+  case $ensure {
+    present: {
+      augeas { "sshd_config_${name}":
+        changes => "set ${name} ${value}",
+        onlyif  => "get ${name} != ${value}",
+      }
+    }
+    add: {
+      augeas { "sshd_config_${name}":
+        onlyif  => "get ${name}[. = '${value}'] != ${value}",
+        changes => [
+          "ins ${name} after ${name}[last()]",
+          "set ${name}[last()] ${value}"
+        ],
+      }
+    }
+    absent: {
+      augeas { "sshd_config_${name}":
+        changes => "rm ${name}",
+        onlyif  => "get ${name}",
+      }
+    }
+    default: {
+      fail("ensure value must be present, add or absent, not ${ensure}")
+    }
+  }
+}
